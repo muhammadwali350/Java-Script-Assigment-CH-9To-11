cd58c180c7189e48deee17de008bbcf2956b9d35
mohammadwalikhan <mohammadwalikhan1@github.com>
2021-05-04 01:31:05 +0500

    assignment completed

---
+++ script.js
@@ -0,0 +1,235 @@
+// ---------------CITY-------------- //
+// var city = prompt('Please enter your city');
+// if("karachi".toLocaleUpperCase);
+// alert('WElcome to City Of Lights');
+// -------------Gender----------- //
+// var gender = prompt("Enter your Gender please ?")
+// if (gender.toUpperCase() == "male".toUpperCase()) {
+//     alert("Good Morning Sir")
+// }
+//     else if (gender.toUpperCase() == 
+//     "female".toUpperCase()) {
+//         alert("Good Morning Ma’am")
+//     }
+
+// ---------------Signal Message------------- //
+// var color = prompt("Enter any color of traffic signal")
+// if (color.toUpperCase() == "red".toUpperCase()){
+//     alert('Vehicle Must Stop')
+// };
+//     if (color.toUpperCase() == "yellow".toUpperCase()){    
+//     alert('vehicle should get ready to move')
+// };
+
+//     if (color.toUpperCase() == "green".toUpperCase()){
+//     alert('vehicle can move now')
+// };
+// ---------Car Fuel------------ //
+// var  fuelQuantity = +prompt("enter the amount of remaining fuel in liters")
+
+
+// if(fuelQuantity < 0.25) {
+//     alert("PLEASE REFILL YOUR FUEL TANK ASAP!")
+// }
+
+// __________task 5__________
+
+// var a = 4;
+// if (++a === 5){
+// alert("given condition for variable a is true");
+// }
+
+
+// var b = 82;
+// if(b++ === 83){
+// alert("given condition for variable b is true");
+// }
+
+
+// var c = 12;
+// if (c++ === 13){
+// alert("condition 1 is true");
+// }
+// if (c === 13){
+// alert("condition 2 is true");
+// }
+// if (++c < 14){
+// alert("condition 3 is true");
+// }
+// if(c === 14){
+// alert("condition 4 is true");
+// }
+
+
+// var materialCost = 20000;
+// var laborCost = 2000;
+// var totalCost = materialCost + laborCost;
+// if (totalCost === laborCost + materialCost){
+// alert("The cost equals");
+// }
+
+
+// if (true){
+//     alert("True");
+//     }
+//     if (false){
+//     alert("False");
+//     }
+
+
+// if("car" < "cat"){
+//     alert("car is smaller than cat");
+//     }
+
+
+
+// _____________task 6_____________
+
+
+// var a =[];
+// var b =0;
+// var c ;
+// var d= 300;
+// var remarks;
+// var grade;
+// for (var aa=1; aa<=3; aa++){
+//     a[aa] = +prompt("enter a marks of three subject");
+//     b=b+a[aa];
+// }
+// c=(b / d)*100;
+// if (c==0 && c<45){
+//    grade="F";
+//    remarks = "fail :";
+//    document.write("<h1> MARK SHEET  <br>");
+//     document.write("TOTAL MARKS:" + " " + d +"<br>");
+//     document.write("OBTAINED MARKS:" + " " + b + "<br>");
+//     document.write("PERCENTAGE:" + " " + c + "<br>");
+//     document.write("GRADE:" + " " + grade + "<br>");
+//     document.write("REMARKS:" + " " + remarks + "<br> </h1>");
+// }
+// else if (c>45 && c<55){
+//   grade="c";
+//   remarks= "pass : doing hard work";
+//   document.write("<h1> MARK SHEET  <br>");
+//     document.write("TOTAL MARKS:" + " " + d +"<br>");
+//     document.write("OBTAINED MARKS:" + " " + b + "<br>");
+//     document.write("PERCENTAGE:" + " " + c + "<br>");
+//     document.write("GRADE:" + " " + grade + "<br>");
+//     document.write("REMARKS:" + " " + remarks + "<br> </h1>");
+// }
+// else if (c>55 && c<65){
+//     grade = "B";
+//     remarks= "pass : good";
+//     document.write("<h1> MARK SHEET  <br>");
+//     document.write("TOTAL MARKS:" + " " + d +"<br>");
+//     document.write("OBTAINED MARKS:" + " " + b + "<br>");
+//     document.write("PERCENTAGE:" + " " + c + "<br>");
+//     document.write("GRADE:" + " " + grade + "<br>");
+//     document.write("REMARKS:" + " " + remarks + "<br> </h1>");
+// }
+// else if (c>65 && c<80){
+//     grade = "A";
+//     remarks= "pass : smart boy";
+//     document.write("<h1> MARK SHEET  <br>");
+//     document.write("TOTAL MARKS:" + " " + d +"<br>");
+//     document.write("OBTAINED MARKS:" + " " + b + "<br>");
+//     document.write("PERCENTAGE:" + " " + c + "<br>");
+//     document.write("GRADE:" + " " + grade + "<br>");
+//     document.write("REMARKS:" + " " + remarks + "<br> </h1>");
+// }
+// else if (c>80 && c<=100){
+//     grade = "A+";
+//     remarks= "pass : king";
+//     document.write("<h1> MARK SHEET  <br>");
+//     document.write("TOTAL MARKS:" + " " + d +"<br>");
+//     document.write("OBTAINED MARKS:" + " " + b + "<br>");
+//     document.write("PERCENTAGE:" + " " + c + "<br>");
+//     document.write("GRADE:" + " " + grade + "<br>");
+//     document.write("REMARKS:" + " " + remarks + "<br> </h1>");
+// }
+// else{
+//     document.write("enter corrects marks");
+// }
+
+
+// _____________task 7_____________
+
+
+
+
+// var secret = 8.8;
+// var se = +prompt("enter a secret number");
+// if (se === secret){
+//     document.write("bingo ,correct");
+// }
+// else if (se === secret){
+//     document.write("close enough");
+// }
+// else {
+//     document.write("wrong");
+// }
+
+
+
+// _____________task 8_____________
+
+
+//  var divisible = +prompt("enter a number");
+//  if (divisible%3===0){
+//       document.write("number is divisible by 3");
+//  }
+// else{
+//     document.write("its not divisible by 3");
+// }
+
+
+// _____________task 10_____________
+
+
+// var givenTemp = +prompt("Enter the temperature you know.")
+
+// if (givenTemp > 40) {
+//     document.write("<h2>The temperature is too hot outside</h2>")
+// }
+
+// else if (givenTemp > 30) {
+//     document.write("<h2>The Weather today is Normal.</h2>")
+// }
+
+// else if (givenTemp > 20) {
+//     document.write("<h2>Today’s Weather is cool.</h2>")
+// }
+
+// else if (givenTemp > 10) {
+//     document.write("<h2>OMG! Today’s weather is so Cool.</h2>")
+// }
+
+
+// _____________task 9_____________
+
+// var no = +prompt("enter a number");
+// if (no%2===0){
+//     document.write("even num");
+// }
+// else {
+//     document.write("odd no");
+// }
+
+// _____________task 11_____________
+
+
+// var no1 = +prompt("enter no 1");
+// var no2 = +prompt("enter no2");
+// var sym = prompt("enter what you do in calculator");
+// if (sym== "+"){
+//     document.write("the addition is :" + " " + (no1+no2));
+// }
+// else if (sym== "-"){
+//     document.write("the subtraction is :" + " " + (no1-no2));
+// }
+// else if (sym== "*"){
+//     document.write("the multiplication is :" + " " + no1*no2);
+// }
+// else if (sym== "/"){
+//     document.write("the division  is :" + " " + no1/no2);
+// }
